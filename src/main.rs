40f0be7578eaeadbf176057219f3a55cdc8cc9af
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2023-08-08 00:05:54 +0200

    add env_logger to main.rs
    adds env_logger to the main.rs file to enable logging for the program. this includes adding the env_logger dependency to cargo.toml and adding the necessary initialization code to main.rs.
    
    the diff shows the changes to cargo.lock and cargo.toml to add the env_logger dependency, as well as the changes to src/main.rs to include the initialization code.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use askama::Template;
 use chrono::{NaiveDate, Utc};
 use clap::Parser;
+use env_logger::Builder;
 use futures::executor::block_on;
+use log::info;
 
 mod github_graphql;
 
@@ -35,16 +37,19 @@ struct Changelog {
   labels: String,
 }
 
-fn main() {
-  let args: Args = Args::parse();
+fn init_logger() {
+  Builder::new().filter(None, log::LevelFilter::Info).init();
+}
 
+fn main() {
+  init_logger();
+  let args: Args = Args::parse();
   let future = github_graphql::get_pull_requests(
     &args.owner,
     &args.project,
     &args.release,
     &args.github_token,
   );
-
   let pull_requests = block_on(future);
   let pr_markdown = github_graphql::format_pull_requests_to_md(&pull_requests);
   let contributors = github_graphql::format_contributors_to_md(&pull_requests);
@@ -59,6 +64,5 @@ fn main() {
     contributors,
     labels,
   };
-
-  println!("{}", changelog.render().unwrap());
+  info!("changelog: {}", changelog.render().unwrap());
 }
